2808e1f8bd903dd03ea18cc57130efc92c015bae
Thomas Alcala Schneider <thomas@lockrmail.com>
2024-05-27 09:08:20 +0200

    feat: adding colorscheme

---
+++ lua/custom/plugins/colorschemes/lackluster.lua
@@ -0,0 +1,9 @@
+return {
+    "slugbyte/lackluster.nvim",
+    -- init = function()
+    --     vim.cmd.colorscheme("lackluster")
+    --     -- vim.cmd.colorscheme("lackluster-night")
+    --     -- vim.cmd.colorscheme("lackluster-mint")
+    --     -- vim.cmd.colorscheme("lackluster-dark")
+    -- end,
+}
